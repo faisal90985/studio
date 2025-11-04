160cbfb90250a56f1fe3b0f99fed56a3c20b9925
Muhammad Faisal <feelfreetocontact90985@gmail.com>
2025-11-04 07:28:53 +0000

    please set the storage bucket value in my app

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:475508111197:web:bc1ef880a0751aadb61ab4",
   "apiKey": "AIzaSyA_ocvbPx61KOXKHenmmaPLT2YR9T5ktbo",
   "authDomain": "studio-6555756591-a79c8.firebaseapp.com",
+  "storageBucket": "studio-6555756591-a79c8.appspot.com",
   "measurementId": "",
   "messagingSenderId": "475508111197"
 };
